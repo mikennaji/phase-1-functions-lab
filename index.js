0a6f7e67f9f3ec92267276bf1651217b181e8860
Michael Tobie Nnaji <tobiennaji@gmail.com>
2022-03-17 17:37:55 -0400

    commit tree 576e673fcd3bcb36575cc1f77cb93f744e549475

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    const distance = street - 42;
+    return Math.abs(distance);
+}
+
+function distanceFromHqInFeet(street){
+    const blocks = distanceFromHqInBlocks(street);
+    return (blocks *264);
+}
+
+function distanceTravelledInFeet(start, destination){
+    const coveredDistance = start - destination;
+    return Math.abs(coveredDistance)*264;
+}
+
+function calculatesFarePrice(start,destination){
+    const distance = distanceTravelledInFeet(start,destination);
+    if(distance>0  &&  distance <= 400){
+        return 0;
+    }
+    else if(distance>400 && distance <= 2000){
+        return (0.02 * (distance-400));
+    }
+    else if (distance > 2000 && distance < 2500){
+        return(25);
+    }
+    else{
+        return "cannot travel that far";
+    }
+}
